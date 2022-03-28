33c6b0195b7774c2e240470ff30e21c56305a121
Kaan Alptakin <kaanalptkn@icloud.com>
2022-03-28 10:02:14 +0000

    Structred main templates, folders, and files

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "Hello World ... again!"
+def home():
+    recipes = mongo.db.recipes.find()
+    return render_template("home.html", recipes=recipes)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
