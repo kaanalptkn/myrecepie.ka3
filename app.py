7d1806ef02720db8cd8ac4936aa390423a8278b0
Kaan Alptakin <kaanalptkn@icloud.com>
2022-03-29 10:16:31 +0000

    Add materialzie navbar and sidenav, add categories

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import (
     Flask, flash, render_template, 
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -18,10 +20,24 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+@app.route("/get_index")
 def home():
-    recipes = mongo.db.recipes.find()
-    return render_template("home.html", recipes=recipes)
+    """
+    This is function for main page
+    """
+    return render_template("index.html")
 
+@app.route("/get_categories")
+def get_categories():
+    """
+    This is function for to get all categories from MongoDB
+    """
+    categories = mongo.db.categories.find()
+    recipes = mongo.db.recipes.find()
+    return render_template(
+                            "categories.html", 
+                            categories=categories, 
+                            recipes=recipes)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
