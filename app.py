85548f4273cc03d95c20206696ba4f033e2558bf
Kaan Alptakin <kaanalptkn@icloud.com>
2022-04-23 11:12:02 +0000

    Deploy to Heroku via CLI

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import (
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
+
 if os.path.exists("env.py"):
     import env
 
